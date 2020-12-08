f6b79e225b7984a62b1fbfd25465653885db275a
austin <austinpoon615@gmail.com>
2020-12-08 21:41:22 +0800

    feat(k-mean): k-mean

--- src/clustering/K-Mean_Cluster_Mapper.py
+++ src/clustering/K-Mean_Cluster_Mapper.py
@@ -3,6 +3,7 @@
 import sys
 from shapely.geometry import Point, Polygon
 
+
 def is_float(val):
     try:
         num = float(val)
@@ -11,14 +12,19 @@ def is_float(val):
     return True
 
 
-centroids = [Point((40.595231, -74.124360)), Point((40.691403, -73.906963)), Point((40.796549, -73.946916))]
+centroids = [Point((40.595231, -74.124360)),
+             Point((40.625425, -73.981473)),
+             Point((40.691403, -73.906963)),
+             Point((40.796549, -73.946916)),
+             Point((40.862269, -73.861293))]
 
 for data in sys.stdin:
     data = data.strip()
     record = data.split(',')
     latitude = record[len(record)-9]
     longitude = record[len(record)-8]
-    if is_float(latitude) and is_float(longitude):
+    category = record[len(record)-24]
+    if category == 'FELONY' and is_float(latitude) and is_float(longitude):
         point = Point((float(latitude), float(longitude)))
         clostestCentroid = centroids[0]
         clostestDistance = point.distance(centroids[0])
@@ -27,4 +33,5 @@ for data in sys.stdin:
             if distance < clostestDistance:
                 clostestCentroid = centroid
                 clostestDistance = distance
-        sys.stdout.write("%s,%s\t%s,%s\n" % (clostestCentroid.x, clostestCentroid.y, point.x, point.y))
+        sys.stdout.write("%s,%s\t%s,%s\n" % (
+            clostestCentroid.x, clostestCentroid.y, point.x, point.y))
